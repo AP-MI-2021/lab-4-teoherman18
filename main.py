6ad24c11fe13cc2dfc6bf27fc8f91c384c35a2a9
teoherman18 <ioana.herman@stud.ubbcluj.ro>
2021-10-18 15:47:40 +0300

    primele 3

--- main.py
+++ main.py
@@ -28,11 +28,29 @@ def test_afisare_lista():
     assert afisare_lista([]) == []
     assert afisare_lista([12,15,12,155,18,20,12,15]) == [12,15,155,18,20]
 
+def suma_primelor_n_numere_pozitive(lst,n):
+    """
+    Calculeaza suma primelor n numere pozitive din lista
+    :param lst: lista de numere intregi
+    :param n: numarul citit de la tastatura
+    :return: suma primelor n numere pozitive din lista
+    """
+    suma = 0
+    for i in lst:
+        if i >= 0 and n != 0:
+            suma = suma + i
+            n = n-1
+    return suma
+
+
+def test_suma_primelor_n_numere_pozitive():
+    assert suma_primelor_n_numere_pozitive([2,3,4,5,-2,-1],3) == 9
+    assert suma_primelor_n_numere_pozitive([0,2,3,-1,5],3) == 5
 
 def Print_Menu():
     print('1. Citire lista')
     print('2. Afiseaza lista dupa eliminarea duplicatelor')
-    print('3. ')
+    print('3. Calculeaza suma primelor n numere pozitive din lista')
     print('4. ')
     print('5.')
     print('x. Iesire')
@@ -40,6 +58,7 @@ def Print_Menu():
 
 def main():
     test_afisare_lista()
+    test_suma_primelor_n_numere_pozitive()
     l = []
     while True:
 
@@ -50,7 +69,12 @@ def main():
         elif optiune == "2":
             print(afisare_lista(lst))
         elif optiune == "3":
-            print()
+            n=int(input('Dati numarul: '))
+            suma_primelor_n_numere_pozitive(lst,n)
+            if n > 0:
+                print('Dimensiunea listei este prea mica')
+            elif suma_primelor_n_numere_pozitive(lst,n) != 0:
+                print(suma_primelor_n_numere_pozitive(lst,n))
         elif optiune == "4":
             print()
         elif optiune == "5":
